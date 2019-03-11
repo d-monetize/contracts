4b79bd898fff0390dd1275d4e71dbfd001406df4
t4sk <tsk.nakamura@gmail.com>
2019-03-11 19:07:38 +0900

    link library in migration

--- migrations/3_deploy_test_contracts.js
+++ migrations/3_deploy_test_contracts.js
@@ -1,8 +1,10 @@
+var Set = artifacts.require("./Set.sol")
 var TestAddressSet = artifacts.require("./TestAddressSet.sol")
 var TestToken = artifacts.require("./TestToken.sol")
 
 module.exports = function(deployer, network) {
   if (network !== "main") {
+    deployer.link(Set, TestAddressSet)
     deployer.deploy(TestAddressSet)
     deployer.deploy(TestToken)
   }
