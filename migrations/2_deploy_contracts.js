1713e9df150676171fc42a590eb6573e83cbc245
t4sk <tsk.nakamura@gmail.com>
2019-03-12 17:57:48 +0900

    remove Set lib from migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,7 @@
-var Set = artifacts.require("./Set.sol")
 var SubscriptionRegistry = artifacts.require("./SubscriptionRegistry.sol")
 var PaymentBounty = artifacts.require("./PaymentBounty.sol")
-var Subscription = artifacts.require("./Subscription.sol")
 
 module.exports = function(deployer) {
-  deployer.deploy(Set)
-  deployer.link(Set, [SubscriptionRegistry, PaymentBounty, Subscription])
   deployer.deploy(PaymentBounty)
   deployer.deploy(SubscriptionRegistry)
 }
--- migrations/3_deploy_test_contracts.js
+++ migrations/3_deploy_test_contracts.js
@@ -1,11 +1,7 @@
-var Set = artifacts.require("./Set.sol")
-var TestAddressSet = artifacts.require("./TestAddressSet.sol")
 var TestToken = artifacts.require("./TestToken.sol")
 
 module.exports = function(deployer, network) {
   if (network !== "main") {
-    deployer.link(Set, TestAddressSet)
-    deployer.deploy(TestAddressSet)
     deployer.deploy(TestToken)
   }
 }
