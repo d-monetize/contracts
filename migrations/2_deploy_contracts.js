cb670e9ae474d8d2acbd3e21a24d7715f3459b18
t4sk <tsk.nakamura@gmail.com>
2019-03-16 15:57:47 +0900

    transfer PaymentBounty ownership to SubscriptionRegistry in migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,13 @@
 var SubscriptionRegistry = artifacts.require("./SubscriptionRegistry.sol")
 var PaymentBounty = artifacts.require("./PaymentBounty.sol")
 
-module.exports = function(deployer) {
-  deployer.deploy(PaymentBounty)
-  deployer.deploy(SubscriptionRegistry)
+module.exports = async function(deployer) {
+  await deployer.deploy(PaymentBounty)
+  await deployer.deploy(SubscriptionRegistry, PaymentBounty.address)
+
+  // transfer PaymentBounty ownership
+  const paymentBounty = await PaymentBounty.deployed()
+  const subscriptionRegistry = await SubscriptionRegistry.deployed()
+
+  await paymentBounty.transferOwnership(subscriptionRegistry.address)
 }
