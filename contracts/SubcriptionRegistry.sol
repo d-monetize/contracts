648f3bf0836583d0664bd571c2c32c1a90c9a10a
t4sk <tsk.nakamura@gmail.com>
2019-05-23 21:12:49 +0900

    new subscription registry

--- contracts/SubcriptionRegistry.sol
+++ contracts/SubcriptionRegistry.sol
@@ -1,145 +1,26 @@
 pragma solidity 0.5.2;
 
 import "./Subscription.sol";
-import "./PaymentBounty.sol";
-
-// TODO subscriptions created count
-// TODO subscribed count
-// TODO payment processor delegation
 
 contract SubscriptionRegistry {
   event SubscriptionCreated(
-    address indexed payee,
     address indexed subscription,
-    address indexed token,
-    uint amount,
-    uint interval
+    address indexed owner,
+    address indexed token
   );
-  event SubscriptionDeleted(
-    address indexed payee,
-    address indexed subscription
-  );
-  event SubscriptionPaused(address indexed subscription);
-  event SubscriptionUnpaused(address indexed subscription);
-  event Subscribed(
-    address indexed payee,
-    address indexed subscription,
-    address indexed subscriber
-  );
-  event Unsubscribed(
-    address indexed payee,
-    address indexed subscription,
-    address indexed subscriber
-  );
-  event BountyRegistered(address indexed subscription);
-  event BountyUnregistered(address indexed subscription);
 
-  PaymentBounty public paymentBounty;
+  address[] public subscriptions;
 
-  mapping(address => address) public ownerOf;
-
-  constructor(address _paymentBounty) public {
-    paymentBounty = PaymentBounty(_paymentBounty);
-  }
-
-  modifier onlyPayee(address payee, address subscription) {
-    require(
-      ownerOf[subscription] == payee,
-      "Not the payee of subscripiton"
+  function create(address token, uint interval, uint amount) public {
+    Subscription subscription = new Subscription(
+      msg.sender, token, amount, interval
     );
-    _;
+
+    subscriptions.append(subscription);
+
+    emit SubscriptionCreated(address(subscription), msg.sender, token);
   }
 
-  modifier onlyRegistered(address subscription) {
-    require(isRegistered(subscription), "Subscription is not registered");
-    _;
-  }
-
-  function isRegistered(address subscription) public view returns (bool) {
-    return ownerOf[subscription] != address(0);
-  }
-
-  function createSubscription(address token, uint amount, uint interval)
-    public
-  {
-    Subscription sub = new Subscription(msg.sender, token, amount, interval);
-
-    address subscription = address(sub);
-    ownerOf[subscription] = msg.sender;
-
-    emit SubscriptionCreated(msg.sender, subscription, token, amount, interval);
-  }
-
-  function deleteSubscription(address subscription)
-    public
-    onlyPayee(msg.sender, subscription)
-  {
-    Subscription sub = Subscription(subscription);
-
-    delete ownerOf[subscription];
-    sub.kill();
-
-    if (paymentBounty.isRegistered(subscription)) {
-      paymentBounty.unregister(subscription);
-    }
-
-    emit SubscriptionDeleted(msg.sender, subscription);
-  }
-
-  function pauseSubscription(address subscription)
-    public
-    onlyPayee(msg.sender, subscription)
-  {
-    Subscription sub = Subscription(subscription);
-    sub.pause();
-
-    emit SubscriptionPaused(subscription);
-  }
-
-  function unpauseSubscription(address subscription)
-    public
-    onlyPayee(msg.sender, subscription)
-  {
-    Subscription sub = Subscription(subscription);
-    sub.unpause();
-
-    emit SubscriptionUnpaused(subscription);
-  }
-
-  function subscribe(address subscription) public onlyRegistered(subscription) {
-    Subscription sub = Subscription(subscription);
-
-    sub.subscribe(msg.sender);
-
-    emit Subscribed(sub.payee(), subscription, msg.sender);
-  }
-
-  function unsubscribe(address subscription)
-    public
-    onlyRegistered(subscription)
-  {
-    Subscription sub = Subscription(subscription);
-
-    sub.unsubscribe(msg.sender);
-
-    emit Unsubscribed(sub.payee(), subscription, msg.sender);
-  }
-
-  function registerBounty(address subscription, uint reward)
-    public
-    onlyPayee(msg.sender, subscription)
-  {
-    paymentBounty.register(subscription, reward);
-
-    emit BountyRegistered(subscription);
-  }
-
-  function unregisterBounty(address subscription)
-    public
-    onlyPayee(msg.sender, subscription)
-  {
-    paymentBounty.unregister(subscription);
-
-    emit BountyUnregistered(subscription);
-  }
+  // TODO get subscription count
+  // TODO get subscriptions
 }
