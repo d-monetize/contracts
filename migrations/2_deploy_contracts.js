e079ec684fcf3921d6eebed14672e5aeca89d7be
t4sk <tsk.nakamura@gmail.com>
2019-05-26 13:06:35 +0900

    migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,5 @@
 var SubscriptionRegistry = artifacts.require("./SubscriptionRegistry.sol")
-var PaymentBounty = artifacts.require("./PaymentBounty.sol")
 
 module.exports = async function(deployer) {
-  await deployer.deploy(PaymentBounty)
-  await deployer.deploy(SubscriptionRegistry, PaymentBounty.address)
-
-  // transfer PaymentBounty ownership
-  const paymentBounty = await PaymentBounty.deployed()
-  const subscriptionRegistry = await SubscriptionRegistry.deployed()
-
-  await paymentBounty.transferOwnership(subscriptionRegistry.address)
+  await deployer.deploy(SubscriptionRegistry)
 }
