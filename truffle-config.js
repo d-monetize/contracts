536d2c32d15478d383f4d2c3f466a0813d1effa8
t4sk <tsk.nakamura@gmail.com>
2019-08-18 15:16:06 +0900

    install mythx

--- truffle-config.js
+++ truffle-config.js
@@ -28,6 +28,9 @@ require("dotenv").config()
 const HDWalletProvider = require("truffle-hdwallet-provider")
 
 module.exports = {
+
+  plugins: ["truffle-security"],
+
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
